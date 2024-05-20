491f895399e055798497a31f8d4a06125d085e7d
AKonicki26 <anne.konicki@mymail.champlain.edu>
2024-05-20 17:45:53 -0400

    Add day 1 progress

---
+++ src/main/java/com/example/items/MugCan.java
@@ -0,0 +1,9 @@
+package com.example.items;
+
+import net.minecraft.item.Item;
+
+public class MugCan extends Item {
+    public MugCan(Settings settings) {
+        super(settings);
+    }
+}
---
+++ src/main/java/com/example/items/MugEssence.java
@@ -0,0 +1,9 @@
+package com.example.items;
+
+import net.minecraft.item.Item;
+
+public class MugEssence extends Item {
+    public MugEssence(Settings settings) {
+        super(settings);
+    }
+}
