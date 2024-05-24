bfeb882e252085729b2cfcaf1c82c1388179e4c6
AKonicki26 <anne.konicki@mymail.champlain.edu>
2024-05-24 18:05:40 -0400

    Mug Can now gives slowness

--- src/main/java/com/mugmod/item/MugCan.java
+++ src/main/java/com/mugmod/item/MugCan.java
@@ -1,7 +1,26 @@
 package com.mugmod.item;
 
+import com.mugmod.MugMod;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
 public class MugCan extends ModItem {
     public MugCan(Settings settings) {
         super(settings, "mug_can");
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (user instanceof PlayerEntity) {
+            //((PlayerEntity) user).getInventory().clear();
+            //user.kill();
+            user.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 200, 20));
+        }
+        return super.finishUsing(stack, world, user);
     }
 }
