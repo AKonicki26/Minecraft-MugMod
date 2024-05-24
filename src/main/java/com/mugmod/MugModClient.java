52bac91569eb3ff9c9f0c68af33960a967860fe2
Brendan O'Leary <brendan.oleary@mymail.champlain.edu>
2024-05-24 00:25:17 -0400

    FoodComponents.java Other things maybe i forgor

---
+++ src/main/java/com/mugmod/MugModClient.java
@@ -0,0 +1,10 @@
+package com.mugmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MugModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
